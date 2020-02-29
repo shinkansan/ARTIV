9a2da9c4ccfe078efa11a9d960aff180c02f2358
신관준 <shinkansan@dgist.ac.kr>
2020-02-29 23:57:38 +0900

    Add files via upload

--- ROS/image_sub_py/image_sub_py/image_sub_py.py
+++ ROS/image_sub_py/image_sub_py/image_sub_py.py
@@ -7,17 +7,25 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 from sensor_msgs.msg import Image
 
+currentSpeed = 0.0
+
 def image_callback(msg : Image):
-	print("Image Received", str(msg.header.frame_id))
-	c = np.fromstring(bytes(msg.data), np.uint8)
+	print("Image Received", str(msg.width), str(msg.height), str(msg.header.frame_id))
+	print("Encoding Type:", str(msg.encoding))
+	c = np.array(msg.data).astype(np.uint8)
+	c = np.reshape(c, (msg.height, msg.width, -1))
 
-	print(msg, c)
-
-
+	cv2.putText(c, "Speed: " + str(currentSpeed), (20, msg.height-30), 1, 1.8, (0,255,))
+	
 	cv2.imshow("Image", c)
 
-	if cv2.waitKey(10)==27 : exit(1)
+	cv2.waitKey(1) 
 
+
+def joint_callback(msg : JointState):
+	global currentSpeed
+	currentSpeed = msg.velocity[0]
+	currentSpeed = np.round(currentSpeed, 6)
 
 def main(args = None):
 	rclpy.init(args=None)
@@ -25,6 +33,7 @@ def main(args = None):
 	node = rclpy.create_node('image_sub_py')
 
 	sub = node.create_subscription(Image, '/center_camera/image_color', image_callback)
+	sub2 = node.create_subscription(JointState, '/vehicle/joint_states', joint_callback)
 
 	rclpy.spin(node)
 
