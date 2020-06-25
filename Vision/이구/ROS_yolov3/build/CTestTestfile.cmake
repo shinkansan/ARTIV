e07e489c530fdeedcdba7a57d347d17b353a3e5f
99sphere <59161083+99sphere@users.noreply.github.com>
2020-06-25 19:33:51 +0900

    Add files via upload

---
+++ Vision/이구/ROS_yolov3/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/vision/catkin_ws/src
+# Build directory: /home/vision/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
+subdirs("usb_cam")
+subdirs("yolov3_pytorch_ros")
