cfd03941d2355e6ab8ca591fd7926e72d801a1bd
Junsang Ryu <50894726+js-ryu@users.noreply.github.com>
2020-07-08 00:21:40 +0900

    Update device_setting.sh

--- Sensors/setting/device_setting.sh
+++ Sensors/setting/device_setting.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /etc/udev/rules.d
-sudo echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="AK05MK9T", MODE = "0777", SYMLINK+="artivIMU"' > 50-usb-imu.rules
+sudo echo 'ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="AK05MK9T", MODE = "0777", SYMLINK+="artivIMU"' > 50-usb-imu.rules
 
-sudo echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="A9B69WM5", MODE = "0777", SYMLINK+="artivGPS"' > 51-usb-gps.rules
+sudo echo 'ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", ATTRS{serial}=="A9B69WM5", MODE = "0777", SYMLINK+="artivGPS"' > 51-usb-gps.rules
 
 #udevadm control --reload-rules && udevadm trigger
